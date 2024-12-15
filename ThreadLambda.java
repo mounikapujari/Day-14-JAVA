74c0da321b300ccc713fcb7bd5c7d84995b9f510
mounikapujari <mounikapujari2005@gmail.com>
2024-12-15 12:06:30 +0530

    Add files via upload

---
+++ ThreadLambda.java
@@ -0,0 +1,8 @@
+public class ThreadLambda {
+    public static void main(String[] args) {
+        Runnable r =()->System.out.println("i am new from java-8 version");
+        Thread t=new Thread(r);
+        t.start();
+    }
+    
+}
